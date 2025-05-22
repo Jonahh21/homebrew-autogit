e896ba04fe3d9fbf95cbfaeb2eff6f885b178a1c
Juan Francisco Antonaya López <jantonaya@baufest.com>
2025-05-22 12:31:42 +0200

    v0.1.0: Hash añadido

--- autogit.rb
+++ autogit.rb
@@ -2,7 +2,7 @@ class Autogit < Formula
   desc "Gestión automática de versiones semánticas en commits de Git"
   homepage "https://github.com/juanfran-dev/autogit"
   url "https://github.com/juanfran-dev/autogit/releases/download/v1.0.0/autogit-1.0.0.tar.gz"
-  sha256 "ACA_VA_EL_HASH_DEL_TAR_GZ"
+  sha256 "27fd802a9e64806acfacc7dfd9d79afb50216a67922a588bd5b332c401d1a5d5"
   license "MIT"
 
   def install
