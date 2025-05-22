5c93dfd39d794703e997803322f51ad40f3310f3
Juan Francisco Antonaya López <jantonaya@baufest.com>
2025-05-22 13:26:56 +0200

    release v1.3.0

--- Formula/autogit.rb
+++ Formula/autogit.rb
@@ -1,8 +1,8 @@
 class Autogit < Formula
   desc "Gestión automática de versiones semánticas en commits de Git"
   homepage "https://github.com/Jonahh21/homebrew-autogit"
-  url "https://github.com/Jonahh21/homebrew-autogit/releases/download/v1.2.0/autogit-1.2.0.tar.gz"
-  sha256 "64d94e689b7e7e94982f6421db159d58db16b83c35d54734a2a9f0d003ae8130"
+  url "https://github.com/Jonahh21/homebrew-autogit/releases/download/v1.3.0/autogit-1.3.0.tar.gz"
+  sha256 "c0133116d04b2c24c2de231aaefa3af13efbc9644e64c5197af0539e6435fb03"
   license "MIT"
 
   def install
